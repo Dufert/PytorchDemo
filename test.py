8f60059b0e393e7d76a48b5cd35249aae3ad2a8d
Dufert <Dufert1130@gmail.com>
2019-08-06 17:13:53 +0800

    第二次提交

--- test.py
+++ test.py
@@ -1,97 +1,78 @@
-from __future__ import print_function
-import sys
-import os
-import argparse
 import torch
+import torch.utils.data as Data
 import torch.nn as nn
-import torch.backends.cudnn as cudnn
-import torchvision.transforms as transforms
+import torchvision as tv
 from torch.autograd import Variable
-from data import VOC_ROOT, VOC_CLASSES as labelmap
-from PIL import Image
-from data import VOCAnnotationTransform, VOCDetection, BaseTransform, VOC_CLASSES
-import torch.utils.data as data
-from ssd import build_ssd
+import matplotlib.pyplot as plt
 
-parser = argparse.ArgumentParser(description='Single Shot MultiBox Detection')
-parser.add_argument('--trained_model', default='weights/ssd_300_VOC0712.pth',
-                    type=str, help='Trained state_dict file path to open')
-parser.add_argument('--save_folder', default='eval/', type=str,
-                    help='Dir to save results')
-parser.add_argument('--visual_threshold', default=0.6, type=float,
-                    help='Final confidence threshold')
-parser.add_argument('--cuda', default=True, type=bool,
-                    help='Use cuda to train model')
-parser.add_argument('--voc_root', default=VOC_ROOT, help='Location of VOC root directory')
-parser.add_argument('-f', default=None, type=str, help="Dummy arg so we can load in Jupyter Notebooks")
-args = parser.parse_args()
-
-if args.cuda and torch.cuda.is_available():
-    torch.set_default_tensor_type('torch.cuda.FloatTensor')
-else:
-    torch.set_default_tensor_type('torch.FloatTensor')
-
-if not os.path.exists(args.save_folder):
-    os.mkdir(args.save_folder)
+train_data = tv.datasets.MNIST(root="./data_mnist",
+                               train=True,
+                               transform=tv.transforms.ToTensor(), #会将图片数据0-255变成0-1
+                               download=False)
+test_data = tv.datasets.MNIST("./data_mnist", train=False, transform=tv.transforms.ToTensor())
 
 
-def test_net(save_folder, net, cuda, testset, transform, thresh):
-    # dump predictions and assoc. ground truth to text file for now
-    filename = save_folder+'test1.txt'
-    num_images = len(testset)
-    for i in range(num_images):
-        print('Testing image {:d}/{:d}....'.format(i+1, num_images))
-        img = testset.pull_image(i)
-        img_id, annotation = testset.pull_anno(i)
-        x = torch.from_numpy(transform(img)[0]).permute(2, 0, 1)
-        x = Variable(x.unsqueeze(0))
 
-        with open(filename, mode='a') as f:
-            f.write('\nGROUND TRUTH FOR: '+img_id+'\n')
-            for box in annotation:
-                f.write('label: '+' || '.join(str(b) for b in box)+'\n')
-        if cuda:
-            x = x.cuda()
+train_loader = Data.DataLoader(dataset=train_data, batch_size=1000, shuffle=True, num_workers=0)
+test_loader = Data.DataLoader(dataset=test_data, batch_size=200, shuffle=True, num_workers=0)
 
-        y = net(x)      # forward pass
-        detections = y.data
-        # scale each detection back up to the image
-        scale = torch.Tensor([img.shape[1], img.shape[0],
-                             img.shape[1], img.shape[0]])
-        pred_num = 0
-        for i in range(detections.size(1)):
-            j = 0
-            while detections[0, i, j, 0] >= 0.6:
-                if pred_num == 0:
-                    with open(filename, mode='a') as f:
-                        f.write('PREDICTIONS: '+'\n')
-                score = detections[0, i, j, 0]
-                label_name = labelmap[i-1]
-                pt = (detections[0, i, j, 1:]*scale).cpu().numpy()
-                coords = (pt[0], pt[1], pt[2], pt[3])
-                pred_num += 1
-                with open(filename, mode='a') as f:
-                    f.write(str(pred_num)+' label: '+label_name+' score: ' +
-                            str(score) + ' '+' || '.join(str(c) for c in coords) + '\n')
-                j += 1
+test_lable = test_data.test_labels
+
+class VGG16(nn.Module):
+    def __init__(self):
+        super(VGG16,self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=16, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+
+        self.out = nn.Linear(32*7*7, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1)
+        x = self.out(x)
+
+        return x
+cnn = VGG16()
+optimizer = torch.optim.Adam(cnn.parameters(), lr=0.001)
+loss_func = nn.CrossEntropyLoss()
+
+for step, (bx, by) in enumerate(train_loader):
+    bx = Variable(bx)
+    by = Variable(by)
+
+    output = cnn(bx)
+    loss = loss_func(output, by)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
 
 
-def test_voc():
-    # load net
-    num_classes = len(VOC_CLASSES) + 1 # +1 background
-    net = build_ssd('test', 300, num_classes) # initialize SSD
-    net.load_state_dict(torch.load(args.trained_model))
-    net.eval()
-    print('Finished loading model!')
-    # load data
-    testset = VOCDetection(args.voc_root, [('2007', 'test')], None, VOCAnnotationTransform())
-    if args.cuda:
-        net = net.cuda()
-        cudnn.benchmark = True
-    # evaluation
-    test_net(args.save_folder, net, args.cuda, testset,
-             BaseTransform(net.size, (104, 117, 123)),
-             thresh=args.visual_threshold)
+    for test_step, (test_x,test_y) in enumerate(test_loader):
+        test_out = cnn(test_x)
+        pre_y = torch.max(test_out, 1)[1].data.numpy().squeeze()
+        accuracy = sum(pre_y == test_y.numpy()) / 200
+        print(accuracy)
 
-if __name__ == '__main__':
-    test_voc()
+
+
+
+
+
+
+
+
+
+
+
+
+
